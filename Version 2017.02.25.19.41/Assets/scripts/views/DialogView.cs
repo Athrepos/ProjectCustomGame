40796e6faf1a732a486a94509dbba970854d4568
Natanielsr <contatonatanielsoares@gmail.com>
2017-02-25 19:44:20 -0300

    Version 2017.02.26.19.41
    Improved button interface

---
+++ Version 2017.02.25.19.41/Assets/scripts/views/DialogView.cs
@@ -0,0 +1,108 @@
+﻿/*
+   Copyright 2017 Nataniel Soares Rodrigues
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*/
+
+using UnityEngine;
+using System;
+using System.Collections;
+using NatanielSoaresRodrigues.ProjectCustomGame.Controllers;
+using NatanielSoaresRodrigues.ProjectCustomGame.Objs;
+using NatanielSoaresRodrigues.ProjectCustomGame.Utils;
+using UnityEngine.UI;
+
+namespace NatanielSoaresRodrigues.ProjectCustomGame.Views
+{
+	public class DialogView : MonoBehaviour {
+
+		DialogController dialogController;
+		public TypewriterScript typewriterScript;
+
+		public Button[] optionButtons;
+		public GameObject buttons;
+
+
+		// Use this for initialization
+		void Start () {
+
+			dialogController = new DialogController (c_showDialogResponse);
+		}
+
+		void disableButtons(){
+			buttons.SetActive (false);
+			foreach (var btn in optionButtons) {
+				btn.gameObject.SetActive(false);
+			}
+		}
+
+		void c_showDialogResponse(object sender, DialogEventArgs e){
+			
+			if (e.dialogues.Length == 1) {
+				// show a single message
+				disableButtons ();
+				typewriterScript.showMessage("\t"+e.dialogues [0].message);
+				Debug.Log (e.dialogues [0].message);
+			}
+			else if (e.dialogues.Length > 1) { 
+				//show the options to the user
+				buttons.SetActive (true);
+				int i = 0;
+				foreach (var dialog in e.dialogues) {
+					Button btn = optionButtons [i];
+					btn.gameObject.SetActive (true);
+
+					Text txtBtn = btn.GetComponentInChildren<Text> ();
+					txtBtn.text = "\t"+dialog.message;
+
+					Debug.Log (dialog.message);
+					i++;
+				}
+			}
+				
+		}
+
+		public void selectOption(int option)
+		{
+			try {
+				dialogController.selectOption (option);
+			} catch (Exception ex) {
+				Debug.Log (ex.Message);
+			}
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			if (Input.GetKeyUp (KeyCode.Space) ||
+				Input.GetKeyUp(KeyCode.Return)
+			) {
+				try {
+					dialogController.checkDialog ();
+				} catch (Exception ex) {
+					Debug.Log (ex.Message);
+				}
+
+			}
+
+			if (Input.GetKeyUp (KeyCode.Alpha1)) {
+				selectOption (0);
+
+			}
+
+			if (Input.GetKeyUp (KeyCode.Alpha2)) {
+				selectOption (1);
+			}
+		}
+	}
+}

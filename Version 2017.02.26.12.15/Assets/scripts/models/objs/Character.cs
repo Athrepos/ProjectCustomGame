40c95c66ee1066909a92f8f664a0157d75ac0eb2
Natanielsr <contatonatanielsoares@gmail.com>
2017-02-26 12:18:08 -0300

    Version 2017.02.26.12.15
    Implement Character

---
+++ Version 2017.02.26.12.15/Assets/scripts/models/objs/Character.cs
@@ -0,0 +1,16 @@
+﻿
+namespace NatanielSoaresRodrigues.ProjectCustomGame.Objs
+{
+	public class Character {
+
+		public int id {
+			get;
+			set;
+		}
+
+		public string name {
+			get;
+			set;
+		}
+	}
+}

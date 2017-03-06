89639d16fdb8476f2d2db921ced71e72ba9aa944
Natanielsr <contatonatanielsoares@gmail.com>
2017-03-06 19:44:47 -0300

    Version 2017.03.06.19.42
    Character animations

---
+++ Version 2017.03.06.19.42/Assets/scripts/models/xml/MyObjectContainer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+using NatanielSoaresRodrigues.ProjectCustomGame.Objs;
+
+namespace NatanielSoaresRodrigues.ProjectCustomGame.Xml
+{
+	public interface MyObjectContainer
+	{
+
+		MyObject[] myObjects{ get; set;}
+
+
+	}
+}
+

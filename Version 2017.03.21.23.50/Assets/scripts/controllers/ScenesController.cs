42163ec0c9adff40a5544bc4fca38a9bd816f108
Natanielsr <contatonatanielsoares@gmail.com>
2017-03-21 23:51:35 -0300

    Version 2017.03.21.23.51
    animate scene

---
+++ Version 2017.03.21.23.50/Assets/scripts/controllers/ScenesController.cs
@@ -0,0 +1,41 @@
+﻿using NatanielSoaresRodrigues.ProjectCustomGame.Xml;
+using UnityEngine;
+using NatanielSoaresRodrigues.ProjectCustomGame.Objs;
+
+namespace NatanielSoaresRodrigues.ProjectCustomGame.Controllers
+{
+	public class ScenesController : MyObjectController
+	{
+		public Scene CurrentScene {
+			get{ 
+				return current as Scene;
+			}
+			private set{ }
+		}
+
+
+		public ScenesController() : base()
+		{
+			SceneContainer container =  new XmlManagement ("scenes", typeof(SceneContainer)).openFile () as SceneContainer;
+			mainList = container.scenes.ToArray ();
+		}
+
+		public Texture loadTexture(){
+			if (CurrentScene == null)
+				return null;
+			if (CurrentScene.SceneImage == null)
+				return null;
+
+			Texture textureImage = Resources.Load<Texture> ("images/" + CurrentScene.SceneImage);
+
+			return textureImage;
+
+		}
+
+		protected override void mainListOk ()
+		{
+			throw new System.NotImplementedException ();
+		}
+	}
+}
+
---
+++ Version 2017.03.21.23.50/Assets/scripts/models/xml/XmlManagement.cs
@@ -0,0 +1,71 @@
+﻿/*
+   Copyright 2017 Nataniel Soares Rodrigues
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*/
+
+using UnityEngine;
+using System;
+using System.Collections;
+using System.Xml.Serialization;
+using System.IO;
+using System.Collections.Generic;
+using NatanielSoaresRodrigues.ProjectCustomGame.Objs;
+
+//utilizar hashset, para não repetir objetos, para identificar se é igual é preciso implementar o metodo Equals da classe
+
+namespace NatanielSoaresRodrigues.ProjectCustomGame.Xml
+{
+	public class XmlManagement {
+
+		Type type;
+		string fileName;
+
+		public XmlManagement(string fileName, Type type){
+			this.fileName = fileName;
+			this.type = type;
+
+		}
+
+		public object openFile(){
+			TextAsset xml = (TextAsset) Resources.Load("xml/"+fileName);
+
+			object container;
+
+			using(MemoryStream ms = new MemoryStream(xml.bytes)){
+				var serializer = new XmlSerializer (type);
+				container = serializer.Deserialize (ms);
+			}
+
+			return container;
+		}
+
+		void testDialogues(List<Dialog> listD)
+		{
+
+			for (int i = 0; i < listD.Count; i++) {
+				for (int j = i+1; j < listD.Count; j++) {
+					if (listD [i].Id == listD [j].Id)
+						throw new Exception ("There is Duplicate id's in your Dialogues XML file");
+
+					if(listD[i].NextDialog != null && 
+						listD[i].NextDialog.Length > 5)
+						throw new Exception ("The Maximum number of options is 5");
+				}
+			}
+		}
+
+	}
+
+}
